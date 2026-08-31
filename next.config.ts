767b68edf29983b0a22e5a7d73d1cbbc2ef226b4
Sirixo <sirixollc@gmail.com>
2026-09-01 03:40:16 +0530

    fix: next.config.ts serverActions config for version 16.3.3

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverActions: {
-    bodySizeLimit: '12mb',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '12mb',
+    },
   },
 };
 
